d44ae80e4bd09d6ba29fb5f36937529473212886
Chi_Tang <me@chitang.dev>
2023-08-30 14:27:09 +0800

    wssb

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,7 @@ pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 echo "[chaotic-aur]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+pacman -Sy
 
 # Create a user
 useradd -G wheel -m -s /bin/bash builder
