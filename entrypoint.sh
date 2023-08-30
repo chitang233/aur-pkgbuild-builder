ee8a28ddbf780d4096fdb82b27da9db423d4777c
Chi_Tang <me@chitang.dev>
2023-08-30 13:54:43 +0800

    fix user

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,14 @@ export PKGNAME=$INPUT_PACKAGE_NAME
 # Install dependencies
 pacman -Syu --noconfirm git openssh base-devel
 
+# Install paru
+pacman-key --init
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+echo "[chaotic-aur]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+
 # Create a user
 useradd -G wheel -m -s /bin/bash builder
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
