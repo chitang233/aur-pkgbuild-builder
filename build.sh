8cfb97cd208078e8e5fe867863b5cd1d34ba4cec
Chi_Tang <me@chitang.dev>
2023-08-30 13:39:21 +0800

    use prebuilt paru

--- build.sh
+++ build.sh
@@ -18,10 +18,11 @@ git config --global user.name "GitHub Action"
 ssh-keyscan aur.archlinux.org >> ~/.ssh/known_hosts
 
 # Install paru
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si --noconfirm
-cd $HOME
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+echo "[chaotic-aur]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
 
 # Clone the repo
 git clone aur@aur.archlinux.org:${PKGNAME}.git
